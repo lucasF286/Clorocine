c21a9e041104d51d3b730c7541cfa9af110cdae3
Lucas Freitas <lucasfreitas286@outlook.com>
2022-09-13 01:55:53 -0300

    initial commit

---
+++ galeria.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Clorocine</title>
+</head>
+
+<body>
+
+    <nav class="nav-extended  purple lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CLOROCINE</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent purple darken-1">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/u49fzmIJHkb1H4oGFTXtBGgaUS1.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9,3
+                    </p>
+                    <span class="card-title">Vingadores</span>
+                      <p>
+                      Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D.
+                       Nick Fury, o diretor desta agência internacional que mantém a paz, 
+                       logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. 
+                       Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury,
+                       mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.
+                      </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg">
+                    <span class="card-title">Ad Astra</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9,3
+                    </p>
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient
+                        because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/u49fzmIJHkb1H4oGFTXtBGgaUS1.jpg">
+                    <span class="card-title">Vingadores</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9,3
+                    </p>
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient
+                        because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/u49fzmIJHkb1H4oGFTXtBGgaUS1.jpg">
+                    <span class="card-title">Vingadores</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9,3
+                    </p>
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient
+                        because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
